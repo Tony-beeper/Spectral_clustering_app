eecb7201ebe91b40fbbc6bf7e548b2cc10a14282
Youxin Tan <tyx1999120755@gmail.com>
2025-02-05 16:36:33 -0500

    seems to work, let's try

--- app/app.py
+++ app/app.py
@@ -492,4 +492,5 @@ def credit():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # app.run(debug=True, use_reloader=True)
